c0e30bdca30e78afa999eb82500434803b450566
nihelaloulou1 <nihelaloulou6@gmail.com>
2024-12-14 08:55:26 +0100

    udate frontend and backend file

--- enis-app-tp/backend/backend/settings.py
+++ enis-app-tp/backend/backend/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
 'NAME': 'enis_tp',
 'USER': 'dbuser',
 'PASSWORD': 'DBpassword2024',
-'HOST': 'mydb.clgmuqui8nll.us-east-1.rds.amazonaws.com',
+'HOST': DB_HOST ,
 'PORT': 3306,
 }
 
--- enis-app-tp/frontend/src/api.js
+++ enis-app-tp/frontend/src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
-
+import { API_BASE_URL } from "./config";
 const apiUrl = "http";
 
 const api = axios.create({
-  baseURL: "http://44.203.168.225:8000"
+  baseURL:API_BASE_URL
 });
 
 api.interceptors.request.use(
